f39f0f3ef389c673c8a60aeaecdaa1509fac2c0b
sahar231 <Riabi.Sahar@esprit.tn>
2026-04-27 11:56:20 +0100

    feat: création de l'interface FXML pour l'historique des consultations

--- JDBC/src/main/java/controller/HistoriqueController.java
+++ JDBC/src/main/java/controller/HistoriqueController.java
@@ -2,10 +2,18 @@ package controller;
 
 import Entities.Historique;
 import Services.ServiceHistorique;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 import java.util.List;
 
 public class HistoriqueController {
@@ -29,5 +37,16 @@ public class HistoriqueController {
         // On récupère la liste via le service (méthode de récupération avec JOIN)
         List<Historique> liste = service.getHistorique();
         tableHistorique.getItems().setAll(liste);
+    }
+    @FXML
+    void retourListe(ActionEvent event) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/AffichierCoursStudent.fxml"));
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
